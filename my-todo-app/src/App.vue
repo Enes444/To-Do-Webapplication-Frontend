5fce6516242009f0669f9fa64d7a0756adaf3ee4
Enes Kulanoglu <Enes.Kulanoglu@gmail.com>
2024-05-12 21:34:28 +0200

    frontend

--- my-todo-app/src/App.vue
+++ my-todo-app/src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <TodoList />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import TodoList from './components/TodoList.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoList
+    HelloWorld
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Todo List</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        { id: 1, name: 'Abgabe Milestone' },
+        { id: 2, name: 'Vorarbeit informationssicherheit' },
+        { id: 3, name: 'Vorarbeit ' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Hier kommt später der stil, den habe ich noch nicht definiert */
+</style>
