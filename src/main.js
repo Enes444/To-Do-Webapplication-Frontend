d0bbc62e60fd23d68c711227b4d1ad4852fdcec7
Enes Kulanoglu <Enes.Kulanoglu@gmail.com>
2024-06-22 13:30:49 +0200

    Configure Axios base URL using environment variables

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import index from './router';
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://my-springboot-app.onrender.com';
+axios.defaults.baseURL = process.env.VUE_APP_API_URL || 'https://my-springboot-app.onrender.com';
 
 const app = createApp(App);
 app.use(index);
